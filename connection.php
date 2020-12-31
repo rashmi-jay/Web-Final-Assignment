14b5511dcacd4b02dbcae2ea60f98c0ba0a7a590
rashmi-jay <72547640+rashmi-jay@users.noreply.github.com>
2020-12-31 12:40:43 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $server = "localhost";
+    $username = "root";
+    $psw = "";
+    $db = "Sea Side South Park";
+
+    $conn = mysqli_connect("$server","$username","$psw","$db");
+
+?>
